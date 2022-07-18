e94ed21d201a56b79fa4631ac8c7d0f563b17c4c
AmandaAzevedo62 <amandaazevedo62@gmail.com>
2022-07-18 20:14:03 -0300

    add cart.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Cart from "./components/Cart/Cart";
+import styled from "styled-components";
+
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  height: 100%;
+`
+const ContainerFilter = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  width: 25%;
+  height: 100%;
+`
+const ContainerHome = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  width: 50%;
+  height: 100%;
+`
+
+const ContainerCart = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid black;
+  width: 25%;
+  height: 100%;
+`
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <ContainerFilter>
+        Sou Filtro
+      </ContainerFilter>
+      <ContainerHome>
+        Sou Home
+      </ContainerHome>
+      <ContainerCart>
+        <Cart />
+      </ContainerCart>
+    </MainContainer>
   );
 }
 
